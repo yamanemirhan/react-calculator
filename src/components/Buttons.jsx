46fb89b6d3db906232667d27cfaaae365dc48106
Emirhan Yaman <yaman.emrhn@gmail.com>
2022-12-28 17:24:05 +0300

    first commit

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -12,33 +12,33 @@ export default function Buttons({
   return (
     <div className="buttons">
       <div className="btnsLineWrapper">
-        <button onClick={handleOperationBtnClick} className="btn bg-slate-400">
+        <button onClick={handleOperationBtnClick} className="btn bg-gray-300">
           %
         </button>
-        <button onClick={handleClearBtnClick} className="btn bg-rose-400">
+        <button onClick={handleClearBtnClick} className="btn bg-rose-200">
           CE
         </button>
-        <button onClick={handleClearBtnClick} className="btn bg-red-400">
+        <button onClick={handleClearBtnClick} className="btn bg-rose-300">
           C
         </button>
-        <button onClick={handleDelBtnClick} className="btn bg-yellow-200">
+        <button onClick={handleDelBtnClick} className="btn bg-yellow-400">
           DEL
         </button>
       </div>
 
       <div className="btnsLineWrapper">
-        <button onClick={handleOperationBtnClick} className="btn bg-slate-400">
+        <button onClick={handleOperationBtnClick} className="btn bg-gray-300">
           1/X
         </button>
-        <button onClick={handleOperationBtnClick} className="btn bg-slate-400">
+        <button onClick={handleOperationBtnClick} className="btn bg-gray-300">
           x<sup>2</sup>
         </button>
-        <button onClick={handleOperationBtnClick} className="btn bg-slate-400">
+        <button onClick={handleOperationBtnClick} className="btn bg-gray-300">
           <span className="text-sm">2</span>
           <sub>&radic;</sub>
           <sub>x</sub>
         </button>
-        <button onClick={handleOperationBtnClick} className="btn bg-blue-300">
+        <button onClick={handleOperationBtnClick} className="btn bg-indigo-200">
           /
         </button>
       </div>
@@ -46,23 +46,23 @@ export default function Buttons({
       <div className="btnsLineWrapper">
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           7
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           8
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           9
         </button>
-        <button onClick={handleOperationBtnClick} className="btn bg-blue-300">
+        <button onClick={handleOperationBtnClick} className="btn bg-indigo-200">
           X
         </button>
       </div>
@@ -70,23 +70,23 @@ export default function Buttons({
       <div className="btnsLineWrapper">
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           4
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           5
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           6
         </button>
-        <button onClick={handleOperationBtnClick} className="btn bg-blue-300">
+        <button onClick={handleOperationBtnClick} className="btn bg-indigo-200">
           -
         </button>
       </div>
@@ -94,23 +94,23 @@ export default function Buttons({
       <div className="btnsLineWrapper">
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           1
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           2
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           3
         </button>
-        <button onClick={handleOperationBtnClick} className="btn bg-blue-300">
+        <button onClick={handleOperationBtnClick} className="btn bg-indigo-200">
           +
         </button>
       </div>
@@ -121,11 +121,11 @@ export default function Buttons({
         </button>
         <button
           onClick={handleNumBtnClick}
-          className="btn text-white bg-teal-600"
+          className="btn text-white bg-gray-500"
         >
           0
         </button>
-        <button onClick={handleDotBtnClick} className="btn bg-violet-400">
+        <button onClick={handleDotBtnClick} className="btn bg-violet-300">
           .
         </button>
         <button
